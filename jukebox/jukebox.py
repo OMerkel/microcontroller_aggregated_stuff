fd0f8ac530b0c70f30df58dceee9cc784d62489e
Oliver Merkel <Merkel.Oliver@web.de>
2024-12-28 00:54:12 +0100

    Adding song "Die Gedanken sind frei"

--- jukebox/jukebox.py
+++ jukebox/jukebox.py
@@ -11,7 +11,7 @@
 
 # Hardware setup:
 #   Passive buzzer GND to Raspberry Pi Pico GND
-#	Passive buzzer + Pin to GPIO Pin 15
+#   Passive buzzer + Pin to GPIO Pin 15
 
 from machine import Pin, PWM, Timer
 from utime import sleep
@@ -50,28 +50,30 @@ def length_to_duration(length):
     return lookup_duration.index(length)
 
 
-def main(song=scale, speed=0.2, continue_playing=False):
+def main(song=scale, speed=0.2, continue_playing=False, verses=1):
     speaker = setup()
     frequency_mapping = {
         "c": 261, "c#": 277, "d": 293, "e": 329, "f": 349, "f#": 369,
-        "g": 392, "g#": 415, "a": 440, "b": 493,
+        "g": 392, "g#": 415, "a": 440, "a#": 466, "b": 493,
         "C": 523, "C#": 554, "D": 587, "E": 659, "F": 698, "F#": 739,
-        "G": 783, "G#": 830, "A": 880, "B": 987
+        "G": 783, "G#": 830, "A": 880, "A#": 932, "B": 987
     }
     title = song["title"]
     print(f"Playing {title}...")
     while True:
-        for note, length in song["score"]:
-            d = length_to_duration(length)
-            print(note, length, d, end=", ")
-            if note != "-":
-                freq_hz = frequency_mapping[note]
-                speaker.freq(freq_hz)
-                speaker.duty_u16(1000)
-            sleep(0.9 * speed * d)
-            speaker.duty_u16(0)
-            sleep(0.1 * speed * d)
-        print()
+        for verse in range(verses):
+            print(f"Verse #{verse+1}")
+            for note, length in song["score"]:
+                d = length_to_duration(length)
+                print(note, length, d, end=", ")
+                if note != "-":
+                    freq_hz = frequency_mapping[note]
+                    speaker.freq(freq_hz)
+                    speaker.duty_u16(1000)
+                sleep(0.9 * speed * d)
+                speaker.duty_u16(0)
+                sleep(0.1 * speed * d)
+            print()
         speaker.duty_u16(0)
         sleep(2)
         if not continue_playing:
--- jukebox/main.py
+++ jukebox/main.py
@@ -11,14 +11,18 @@
 
 from jukebox import main
 import alle_jahre_wieder
+import die_gedanken_sind_frei
 import froh_und_munter
+import kling_gloeckchen_kling
 import leise_rieselt_der_schnee
 import morgen_kinder_wirds_was_geben as mk
 import silent_night
 
 while True:
-    main(song=alle_jahre_wieder.song, speed=0.15)
-    main(song=leise_rieselt_der_schnee.song)
-    main(song=froh_und_munter.song, speed=0.12)
-    main(song=silent_night.song)
-    main(song=mk.song, speed=0.15, continue_playing=False)
+    main(song=die_gedanken_sind_frei.song, verses=4, speed=0.15)
+    main(song=kling_gloeckchen_kling.song, verses=3)
+    main(song=alle_jahre_wieder.song, verses=3, speed=0.15)
+    main(song=leise_rieselt_der_schnee.song, verses=3)
+    main(song=froh_und_munter.song, verses=5, speed=0.12)
+    main(song=silent_night.song, verses=3)
+    main(song=mk.song, verses=3, speed=0.15, continue_playing=False)
